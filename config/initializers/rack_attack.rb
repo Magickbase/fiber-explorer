4d7e9116b871886fe510aa571792c6b5b636c23c
Rabbit <zhangnan8710@gmail.com>
2025-07-05 19:09:59 +0800

    chore: update rack attack conf

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -54,13 +54,13 @@ class Rack::Attack
   # throttle logins for another user and force their login requests to be
   # denied, but that's not very common and shouldn't happen to you. (Knock
   # on wood!)
-  throttle('logins/email', limit: 5, period: 20.seconds) do |req|
-    if req.path == '/login' && req.post?
-      # Normalize the email, using the same logic as your authentication process, to
-      # protect against rate limit bypasses. Return the normalized email if present, nil otherwise.
-      req.params['email'].to_s.downcase.gsub(/\s+/, "").presence
-    end
-  end
+  # throttle('logins/email', limit: 5, period: 20.seconds) do |req|
+  #   if req.path == '/login' && req.post?
+  #     # Normalize the email, using the same logic as your authentication process, to
+  #     # protect against rate limit bypasses. Return the normalized email if present, nil otherwise.
+  #     req.params['email'].to_s.downcase.gsub(/\s+/, "").presence
+  #   end
+  # end
 
   ### Custom Throttle Response ###
 
